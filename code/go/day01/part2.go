9a417d2b3aa1368fc8053c4201b75ee1afea3229
wboler <wboler@iupui.edu>
2021-12-01 14:09:03 -0500

    Day1, part2, Go

--- code/go/day01/part2.go
+++ code/go/day01/part2.go
@@ -5,15 +5,16 @@ import (
 	utils "./utils"
 )
 
-
 func main() {
 
 	input_filename := utils.Get_args()
 	data := utils.Load_data(input_filename)
-	deltas := utils.Calculate_deltas(data)
+	smooth_data := utils.Smooth_data(data)
+	deltas := utils.Calculate_deltas(smooth_data)
 	upsides := utils.Count_upside(deltas)
 
 	fmt.Printf("Data: %v\n", data)
+	fmt.Printf("Smooth Data: %v\n", smooth_data)
 	fmt.Printf("Deltas: %v\n", deltas)
 	fmt.Printf("Upsides: %v\n", upsides)
 }
