d8f7cfdab03ba54c9ed9dc22dfa5134e4e6e1312
wboler <wboler@iupui.edu>
2021-12-01 13:55:49 -0500

    Day 1, part 1, Go

---
+++ code/go/day01/part1.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"log"
+	"strconv"
+)
+
+func get_args() string {
+	// Get Input File
+	if len(os.Args) < 2 {
+		panic("Error, missing input_filename")
+	}
+	input_filename := os.Args[1]
+	return input_filename
+}
+
+func load_data(input_filename string) []int {
+
+	fmt.Printf("Input Filename: %v\n", input_filename)
+
+	if _, err := os.Stat(input_filename); os.IsNotExist(err) {
+		panic("Error, input file does not exist")
+	}
+
+	file, err := os.Open(input_filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var data []int
+	for scanner.Scan() {
+		v, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		data = append(data, v)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
+
+func calculate_deltas(data []int) []int {
+	if len(data) < 2 {
+		log.Fatal("Error: data less than 2")
+	}
+	
+	deltas := make([]int, len(data) - 1)
+
+	var i int
+	for i =0; i < len(data)-1; i++ {
+		deltas[i] = data[i+1] - data[i]
+	}
+
+	return deltas
+}
+
+func count_upside(deltas []int) int {
+	var upsides, i int
+
+	for i=0; i < len(deltas); i++ {
+		if deltas[i] > 0 {
+			upsides++
+		}
+	}
+	return upsides
+}
+
+func main() {
+
+	input_filename := get_args()
+	data := load_data(input_filename)
+	deltas := calculate_deltas(data)
+	upsides := count_upside(deltas)
+
+	fmt.Printf("Data: %v\n", data)
+	fmt.Printf("Deltas: %v\n", deltas)
+	fmt.Printf("Upsides: %v\n", upsides)
+}
